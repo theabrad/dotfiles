26a5dc985705999ea3445414b79ec99bad4808e7
Alex Bradley <theabrad@users.noreply.github.com>
2022-12-25 01:07:21 -0500

    netrw settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -33,6 +33,7 @@ lsp.on_attach(function(_, bufnr)
   end
 
   nmap("<leader>rn", vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap("<leader>ca", vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap("gd", vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap("gr", require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
--- nvim/lua/theabrad/set.lua
+++ nvim/lua/theabrad/set.lua
@@ -17,5 +17,8 @@ vim.o.smartcase = true
 
 -- netrw settings
 vim.g.netrw_banner = 0
+vim.g.netrw_keepdir = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 30
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
